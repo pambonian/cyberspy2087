8a3abb113fb636a05bfa5bed8fefb9ed8e412b32
Patrick <pambonian@gmail.com>
2023-12-16 22:50:22 -0500

    player movement adjustments

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,10 +17,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerMovement();
+    }
+
+    void PlayerMovement() 
+    {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 movement = x * transform.right + z * transform.forward;
+        movement = movement * speed * Time.deltaTime;
 
         myController.Move(movement);
     }
