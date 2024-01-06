373e35d7992dd5fe3de3720b790b37c1b2da40ee
Patrick <pambonian@gmail.com>
2024-01-05 21:49:59 -0500

    coroutines for full auto or semi auto control

--- Assets/Scripts/GunSystem.cs
+++ Assets/Scripts/GunSystem.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class GunSystem : MonoBehaviour
 {
-
+    // PISTOL
     public Transform myCameraHead;
     public Transform firePosition;
     public GameObject muzzleFlash, bulletHole, waterLeak;
     public GameObject bullet;
+
+    // AUTO FIRE
+    public bool canAutoFire;
+    private bool shooting, readyToShoot = true;
+
+    public float timeBetweenShots;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,43 +30,58 @@ public class GunSystem : MonoBehaviour
 
     private void Shoot()
     {
-        if (Input.GetMouseButtonDown(0))
+        // check the fire rate of the weapon:
+        if(canAutoFire)
         {
+            shooting = Input.GetMouseButton(0);
+        }
+        // if it is not auto-fire, don't treat as auto-fire:
+        else
+        {
+            shooting = Input.GetMouseButtonDown(0);
+        }
+        // if player is actively shooting the weapon:
+        if (shooting && readyToShoot)
+        {
+            readyToShoot = false;
+
             RaycastHit hit;
 
             if (Physics.Raycast(myCameraHead.position, myCameraHead.forward, out hit, 100f))
             {
                 if (Vector3.Distance(myCameraHead.position, hit.point) > 2f)
                 {
-
                     firePosition.LookAt(hit.point);
-
                     if (hit.collider.CompareTag("Shootable"))
                     {
                         Instantiate(bulletHole, hit.point, Quaternion.LookRotation(hit.normal));
                     }
-
                     if (hit.collider.CompareTag("WaterLeaker"))
                     {
                         Instantiate(waterLeak, hit.point, Quaternion.LookRotation(hit.normal));
                     }
                 }
-
                 if (hit.collider.CompareTag("Enemy"))
                 {
                     Destroy(hit.collider.gameObject);
                 }
             }
-
             else
             {
                 firePosition.LookAt(myCameraHead.position + (myCameraHead.forward * 50f));
             }
-
             Instantiate(muzzleFlash, firePosition.position, firePosition.rotation, firePosition);
             Instantiate(bullet, firePosition.position, firePosition.rotation, firePosition);
+
+            StartCoroutine(ResetShot());
         }
     }
 
+    IEnumerator ResetShot()
+    {
+        yield return new WaitForSeconds(timeBetweenShots);
+
+        readyToShoot = true;
+    }
 
 }
