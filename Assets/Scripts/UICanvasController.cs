fb45dcc75252c666522286806e3cfd104e03587e
Patrick <pambonian@gmail.com>
2024-01-14 16:31:02 -0500

    added player health ui system

--- Assets/Scripts/UICanvasController.cs
+++ Assets/Scripts/UICanvasController.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class UICanvasController : MonoBehaviour
 {
 
     public TextMeshProUGUI ammoText, totalAmmoText;
+
+    public Slider healthSlider;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +20,16 @@ public class UICanvasController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        healthSlider.maxValue = health;
+        healthSlider.value = health;
+    }
+
+    public void SetHealth(int health)
+    {
+        healthSlider.value = health;
     }
 }
