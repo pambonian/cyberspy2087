38fd2c531b7f99f8f638bbebe7f385077151b056
Patrick <pambonian@gmail.com>
2024-01-17 20:49:27 -0500

    added a weapons switching system

---
+++ Assets/Scripts/WeaponsSwitchSystem.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponsSwitchSystem : MonoBehaviour
+{
+    private GunSystem activeGun;
+    public List<GunSystem> allGuns = new List<GunSystem>();
+    public int currentGunNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GunSystem gun in allGuns)
+        {
+            gun.gameObject.SetActive(false);
+        }
+
+        activeGun = allGuns[currentGunNumber];
+        activeGun.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            SwitchGun();
+        }
+    }
+
+    private void SwitchGun()
+    {
+        activeGun.gameObject.SetActive(false);
+        currentGunNumber++;
+
+        if (currentGunNumber >= allGuns.Count)
+        {
+            currentGunNumber = 0;
+        }
+
+        activeGun = allGuns[currentGunNumber];
+        activeGun.gameObject.SetActive(true);
+
+    }
+}
