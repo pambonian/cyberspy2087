d746b6e080449e9cf7037cd566dc9ce0f5fe718b
Patrick <pambonian@gmail.com>
2024-01-18 17:56:40 -0500

    rocket ammo causes damage

--- Assets/Scripts/ExplosionDamage.cs
+++ Assets/Scripts/ExplosionDamage.cs
@@ -4,5 +4,29 @@ using UnityEngine;
 
 public class ExplosionDamage : MonoBehaviour
 {
-   
+    public int explosionDamage;
+
+    private void Start()
+    {
+        StartCoroutine(DestroyObject());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<EnemyHealthSystem>().TakeDamage(explosionDamage);
+        }
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerHealthSystem>().TakeDamage(explosionDamage);
+        }
+    }
+
+    IEnumerator DestroyObject()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        Destroy(gameObject);
+    }
 }
