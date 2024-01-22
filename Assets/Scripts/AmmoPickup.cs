e929b10a2dc36b3a29dcb6c559079db418985473
Patrick <pambonian@gmail.com>
2024-01-21 23:51:34 -0500

    init commit

--- Assets/Scripts/AmmoPickup.cs
+++ Assets/Scripts/AmmoPickup.cs
@@ -8,7 +8,6 @@ public class AmmoPickup : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            //
             other.GetComponentInChildren<GunSystem>().AddAmmo();
             Destroy(gameObject);
         }
